2e8f909ea35d96559353b16c6a4f7f7b582d955c
Kyle Barton <kjbarton4@gmail.com>
2018-12-26 20:16:45 -0800

    getting a test framework in place

---
+++ test/position_tests.js
@@ -0,0 +1,9 @@
+let assert = require('assert');
+
+describe('Position Tests', function(){
+    describe('Test the test', function (){
+        it('Should be true', function(){
+            assert.equal(true, 1 === 1);
+        })
+    })
+})
