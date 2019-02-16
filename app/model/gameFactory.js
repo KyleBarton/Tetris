169ca84d50edf4d77b67f8fad2d9d5abf145f8c2
Kyle Barton <kjbarton4@gmail.com>
2019-02-16 13:43:55 -0800

    removing game class, letting gamefactory handle it.

--- app/model/gameFactory.js
+++ app/model/gameFactory.js
@@ -1,6 +1,16 @@
-let Game = require('./game.js');
+let Level = require('./level');
+let pTypes = require('./pieceType');
+
+let pieceService = require('../service/pieceService.js');
+let boardService = require('../service/boardService.js');
+
 module.exports={
     createNewGame: function(){
-        return new Game().startNew();
+        //return new Game().startNew();
+		return { 
+			board: boardService.newBoard(),
+			level: new Level(),
+			newPiece: pieceService.newPiece(pTypes.Cube)
+		}
     }
 }
